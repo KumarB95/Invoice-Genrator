05760a6a983af0427cfdbf37192452f47f6c5893
KumarB95 <kumarbalingal@gmail.com>
2021-11-02 20:51:52 +0530

    Added : Created Invoice generator for min fare value

--- src/test/java/com/bridgelabz/InvoiceGeneratorTest.java
+++ src/test/java/com/bridgelabz/InvoiceGeneratorTest.java
@@ -12,4 +12,12 @@ public class InvoiceGeneratorTest {
         double fare= invoiceGenerator.calculateFare(distance,time);
         Assertions.assertEquals(25,fare,0.0);
     }
+    @Test
+    public void givenLessDistanceTime_ShouldReturnMinFare(){
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        double distance=0.1;
+        int time=1;
+        double fare= invoiceGenerator.calculateFare(distance,time);
+        Assertions.assertEquals(5,fare);
+    }
 }
